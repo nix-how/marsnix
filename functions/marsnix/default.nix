ac58775995aad80040b56dce59a9ee1aa91bed66
matthewcroughan <matt@croughan.sh>
2025-03-18 18:42:46 +0000

    nix.unstable -> nix.latest

--- functions/marsnix/default.nix
+++ functions/marsnix/default.nix
@@ -10,7 +10,7 @@
 }:
 let
   # nix does not instantiated drv files upon nix-env evaluating them, without this patch
-  patchedNix = nixVersions.unstable.overrideAttrs (_: {
+  patchedNix = nixVersions.latest.overrideAttrs (_: {
     prePatch = ''
       substituteInPlace src/nix-env/nix-env.cc \
         --replace 'settings.readOnlyMode = true' 'settings.readOnlyMode = false'
